b85bc36315e8f811c8a4dd2ec2a05cec8d2910d7
Lakshay goyal <akshaybuster5280@gmail.com>
2024-08-13 18:51:54 +0530

    signin setup also done

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,4 +1,32 @@
-const page = () => {
-  return <div>page</div>;
+import SignInFormProvider from "@/components/forms/sign-in/form-provider";
+import LoginForm from "@/components/forms/sign-in/login-form";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+const SignInPage = () => {
+  return (
+    <div className="w-full flex-1 py-36 md:px-16">
+      <div className="flex h-full flex-col gap-3">
+        <SignInFormProvider>
+          <div className="flex flex-col gap-3">
+            <LoginForm />
+            <div className="flex w-full flex-col items-center gap-3">
+              <Button type="submit" className="w-full">
+                Submit
+              </Button>
+              <p>
+                Don’t have an account?{" "}
+                <Link href="/auth/sign-up" className="font-bold">
+                  Create one
+                </Link>
+              </p>
+            </div>
+          </div>
+        </SignInFormProvider>
+      </div>
+    </div>
+  );
 };
-export default page;
+
+export default SignInPage;
