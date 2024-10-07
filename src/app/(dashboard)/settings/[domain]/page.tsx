f2ad1605a77be2e22cda0e1543ab0da62436cc9d
Lakshay goyal <akshaybuster5280@gmail.com>
2024-10-07 18:53:28 +0530

    error fix

--- src/app/(dashboard)/settings/[domain]/page.tsx
+++ src/app/(dashboard)/settings/[domain]/page.tsx
@@ -9,26 +9,25 @@ type Props = { params: { domain: string } };
 
 const DomainSettingsPage = async ({ params }: Props) => {
   const domain = await onGetCurrentDomainInfo(params.domain);
-  // if (!domain) redirect("/dashboard");
-
-  return (
-    <>
-      <InfoBar />
-      <div className="chat-window h-0 w-full flex-1 overflow-y-auto px-3">
-        <SettingsForm
-          plan={domain.subscription?.plan!}
-          chatBot={domain.domains[0].chatBot}
-          id={domain.domains[0].id}
-          name={domain.domains[0].name}
-        />
-        <BotTrainingForm id={domain.domains[0].id} />
-        <ProductTable
-          id={domain.domains[0].id}
-          products={domain.domains[0].products || []}
-        />
-      </div>
-    </>
-  );
+  if (domain)
+    return (
+      <>
+        <InfoBar />
+        <div className="chat-window h-0 w-full flex-1 overflow-y-auto px-3">
+          <SettingsForm
+            plan={domain.subscription?.plan!}
+            chatBot={domain.domains[0].chatBot}
+            id={domain.domains[0].id}
+            name={domain.domains[0].name}
+          />
+          <BotTrainingForm id={domain.domains[0].id} />
+          <ProductTable
+            id={domain.domains[0].id}
+            products={domain.domains[0].products || []}
+          />
+        </div>
+      </>
+    );
 };
 
 export default DomainSettingsPage;
