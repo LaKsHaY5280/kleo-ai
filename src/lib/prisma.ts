cb110a52ddf6c7a5b8c916c478bf89e34b932057
Lakshay goyal <akshaybuster5280@gmail.com>
2024-10-07 19:47:24 +0530

    .

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+
 export const client =
   globalForPrisma.prisma ||
   new PrismaClient({
